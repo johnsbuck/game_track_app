15f45fc0b338a0c48cadf427d39f10dd5ae6c0bc
John Bucknam <jbuck594@gmail.com>
2022-01-03 22:53:11 -0500

    Fixed issue with key

--- gtsite/settings.py
+++ gtsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#r1emp$hql@*)$5u+8xv4mf+7+_&3&=1oqw!=05x!xr%9_fjbw'
+SECRET_KEY = constants.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
